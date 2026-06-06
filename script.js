99b955b437055275ce363381e7a5207a47b3fb99
Samar <samarbabar02@gmail.com>
2026-06-06 18:42:47 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 console.log("Portfolio Loaded");
+/* ACTIVE NAV LINK */
+
+.active-link{
+    color:#06b6d4 !important;
+}
+
+/* SCROLL TOP BUTTON */
+
+.scroll-top{
+    position:fixed;
+    bottom:25px;
+    right:25px;
+
+    width:50px;
+    height:50px;
+
+    border:none;
+    border-radius:50%;
+
+    background:linear-gradient(
+        135deg,
+        #7c3aed,
+        #06b6d4
+    );
+
+    color:white;
+    font-size:22px;
+    cursor:pointer;
+
+    opacity:0;
+    visibility:hidden;
+
+    transition:.3s;
+
+    z-index:1000;
+}
+
+.scroll-top.show{
+    opacity:1;
+    visibility:visible;
+}
+
+.scroll-top:hover{
+    transform:translateY(-5px);
+}
