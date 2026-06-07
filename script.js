65c8a1683cccd506d64fb87761621eed0f209b3f
Samar <samarbabar02@gmail.com>
2026-06-07 21:22:55 +0500

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,30 @@ new Typed("#typing", {
     backSpeed: 40,
     loop: true
 });
+console.log("Portfolio Loaded");
+
+AOS.init({
+    duration: 1000,
+    once: true
+});
+
+new Typed("#typing", {
+    strings: [
+        "Machine Learning Enthusiast",
+        "Data Science Student",
+        "Python Developer",
+        "AI Explorer"
+    ],
+    typeSpeed: 60,
+    backSpeed: 40,
+    loop: true
+});
+
+
+// MOBILE MENU
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
